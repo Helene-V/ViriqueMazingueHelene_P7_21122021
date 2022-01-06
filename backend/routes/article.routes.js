92f1dba6e220c5cba393cd82fb261730feb3cda0
Ln <rainbowliketravel@gmail.com>
2022-01-06 17:06:28 +0100

    modification des routes

--- backend/routes/article.routes.js
+++ backend/routes/article.routes.js
@@ -1,69 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const articleCtrl = require('../controllers/article.controller');
+const auth = require('../middleware/authJwt')
 //const multer = require('../middleware/multer-config');
 
-
-module.exports = app => {
-    const articles = require("../controllers/article.controller.js");
   
-    const router = require("express").Router();
+// Créer un nouvel article
+router.post("/", auth, articleCtrl.create);
   
-    // Créer un nouvel article
-    router.post("/", articles.create);
+// Retrouver tous les articles
+router.get("/", auth, articleCtrl.findAll);
   
-    // Retrouver tous les articles
-    router.get("/", articles.findAll);
+// Retrouver un article par son id
+router.get("/:id", auth, articleCtrl.findOne);
   
-    // Retrouver un article par son id
-    router.get("/:id", articles.findOne);
+// Modifier un article via son id
+router.put("/:id", auth, articleCtrl.update);
   
-    // Modifier un article via son id
-    router.put("/:id", articles.update);
+// Supprimer un article via son id
+router.delete("/:id", auth, articleCtrl.delete);
   
-    // Supprimer un article via son id
-    router.delete("/:id", articles.delete);
+// Supprimer tous les articles
+router.delete("/", auth, articleCtrl.deleteAll);
   
-    // Supprimer tous les articles
-    router.delete("/", articles.deleteAll);
-  
-    app.use('/api/articles', router);
-  };
-
-
-/* Test avec Multer
-
-const multer = require('../middleware/multer-config');
-
-module.exports = app => {
-    const articles = require("../controllers/article.controller.js");
-  
-    const router = require("express").Router();
-  
-    // Créer un nouvel article
-    router.post("/", multer, articles.create);
-  
-    // Retrouver tous les articles
-    router.get("/", articles.findAll);
-  
-    // Retrouver un article par son id
-    router.get("/:id", articles.findOne);
-  
-    // Modifier un article via son id
-    router.put("/:id", multer, articles.update);
-  
-    // Supprimer un article via son id
-    router.delete("/:id", articles.delete);
-  
-    // Supprimer tous les articles
-    router.delete("/", articles.deleteAll);
-  
-    app.use('/api/articles', router);
-  }; */
-
-  /* Récap de l'API :
-  GET --> api/articles --> récupére tous les Articles
-  GET --> api/articles/:id --> récupère l'Article par son id
-  POST --> api/articles --> ajoute un nouvel Article
-  PUT --> api/articles/:id --> modifie un Article récupéré par son id
-  DELETE --> api/articles/:id --> supprime un Article récupéré par son id
-  DELETE --> api/articles --> supprime TOUS les Articles
-  GET --> api/articles?title=[kw] --> trouve tous les Articles dont le titre contient 'kw'
-  */
+module.exports = router;
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -8,21 +8,3 @@ router.post('/signin', userCtrl.signin);
 //router.delete('/:id', auth, userCtrl.delete);
 
 module.exports = router;
-/*const { authJwt, verifySignUp } = require('../middleware');
-const controller = require('../controllers/user.controller');
-
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
-
-  app.post("/api/auth/signup", [verifySignUp.checkDuplicateUsernameOrEmail , authJwt.verifyToken], controller.signup);
-
-  app.post("/api/auth/signin", [authJwt.verifyToken], controller.signin);
-
-  app.delete("/api/:id", controller.delete);
-};*/
